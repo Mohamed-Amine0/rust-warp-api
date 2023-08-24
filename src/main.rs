7af1ca1eadf2d7282669561461a9c4dce2b78206
klibiadam <118206856+klibiadam@users.noreply.github.com>
2023-08-24 15:40:21 +0100

    Update main.rs
    Added verification for the path included as an argument ( existence and format) as well as comment for csv support.

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,43 @@ use crate::utils::*;
 use lazy_static::lazy_static;
 use models::AddDataRequestBody;
 use polars::prelude::*;
+use std::env;
+use std::fs;
 use warp::Filter;
+
+lazy_static! {
+    static ref FILE_PATH: String = {
+        let args: Vec<String> = env::args().collect();
+        args.get(1).unwrap_or(&String::new()).to_string()
+    };
+}
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("MISSING PATH");
         std::process::exit(1);
     }
-    lazy_static! {
-        static ref PARQUET_PATH: String = {
-            let args: Vec<String> = std::env::args().collect();
-            if args.len() < 2 {
-                eprintln!("MISSING PATH");
-                std::process::exit(1);
-            }
-            args[1].clone()
-        };
-    }
 
+    let file_path = &args[1];
+    if file_path.ends_with(".parquet")
+    // || file_path.ends_with(".csv") for csv files support
+    {
+        if fs::metadata(file_path).is_ok() {
+            file_path.to_string();
+        } else {
+            eprintln!("File doesn't exist!");
+            std::process::exit(1);
+        };
+    } else {
+        eprintln!("Invalid file format!");
+        std::process::exit(1);
+    }
     let delete_column_route = warp::path!("delete-column" / String)
         .and(warp::delete())
         .and_then(|column_name: String| async move {
-            delete_column_handler(
-                PARQUET_PATH.to_string(),
-                PARQUET_PATH.to_string(),
-                column_name,
-            )
-            .await
+            delete_column_handler(FILE_PATH.to_string(), FILE_PATH.to_string(), column_name).await
         });
 
     let rename_column_route = warp::path!("rename-column" / String)
@@ -49,8 +57,8 @@ async fn main() {
             |old_column_name: String, new_column_data: Vec<String>| async move {
                 rename_column_handler(
                     old_column_name,
-                    PARQUET_PATH.to_string(),
-                    PARQUET_PATH.to_string(),
+                    FILE_PATH.to_string(),
+                    FILE_PATH.to_string(),
                     new_column_data,
                 )
                 .await
@@ -63,8 +71,8 @@ async fn main() {
         .and_then(
             |column_name: String, request_body: AddDataRequestBody| async move {
                 add_data_to_column_handler(
-                    PARQUET_PATH.to_string(),
-                    PARQUET_PATH.to_string(),
+                    FILE_PATH.to_string(),
+                    FILE_PATH.to_string(),
                     column_name,
                     request_body,
                 )
@@ -76,8 +84,8 @@ async fn main() {
         .and(warp::post())
         .and_then(|column_name, column_type: String| async move {
             add_null_column_handler(
-                PARQUET_PATH.to_string(),
-                PARQUET_PATH.to_string(),
+                FILE_PATH.to_string(),
+                FILE_PATH.to_string(),
                 column_name,
                 column_type,
             )
@@ -88,8 +96,8 @@ async fn main() {
         .and(warp::put())
         .and_then(|column_name, new_type: String| async move {
             cast_column_handler(
-                PARQUET_PATH.to_string(),
-                PARQUET_PATH.to_string(),
+                FILE_PATH.to_string(),
+                FILE_PATH.to_string(),
                 column_name,
                 new_type,
             )
@@ -100,38 +108,28 @@ async fn main() {
         .and(warp::post())
         .and(warp::body::json())
         .and_then(
-            |column_name: String, column_data: Vec<crate::AnyValue<'static>>| {
-                let parquet_path = PARQUET_PATH.clone();
-                let output_path = PARQUET_PATH.clone();
-                async move {
-                    add_column_with_values_handler(
-                        parquet_path,
-                        output_path,
-                        column_name,
-                        column_data,
-                    )
-                    .await
-                }
+            |column_name: String, column_data: Vec<crate::AnyValue<'static>>| async move {
+                add_column_with_values_handler(
+                    FILE_PATH.clone(),
+                    FILE_PATH.clone(),
+                    column_name,
+                    column_data,
+                )
+                .await
             },
         );
 
     let add_row_route = warp::path!("add-row")
         .and(warp::post())
         .and(warp::body::json())
-        .and_then(|new_row_values: Vec<AnyValue<'static>>| {
-            let parquet_path = PARQUET_PATH.clone();
-            let output_path = PARQUET_PATH.clone();
-            async move { add_row_handler(parquet_path, output_path, new_row_values).await }
+        .and_then(|new_row_values: Vec<AnyValue<'static>>| async move {
+            add_row_handler(FILE_PATH.clone(), FILE_PATH.clone(), new_row_values).await
         });
 
     let delete_row_route = warp::path!("delete-row" / u32)
         .and(warp::delete())
         .and_then(move |row_index: u32| {
-            delete_row_handler(
-                PARQUET_PATH.to_string(),
-                PARQUET_PATH.to_string(),
-                row_index,
-            )
+            delete_row_handler(FILE_PATH.to_string(), FILE_PATH.to_string(), row_index)
         });
 
     let update_row_route = warp::path!("update-row" / u32)
@@ -140,27 +138,23 @@ async fn main() {
         .and_then(
             move |row_index: u32, new_row_values: Vec<AnyValue<'static>>| {
                 update_row_handler(
-                    PARQUET_PATH.to_string(),
-                    PARQUET_PATH.to_string(),
+                    FILE_PATH.to_string(),
+                    FILE_PATH.to_string(),
                     row_index,
                     new_row_values,
                 )
             },
         );
 
-    let update_row_counter_route =
-        warp::path!("update-row-counter")
-            .and(warp::put())
-            .and_then(move || {
-                update_row_counter_handler(PARQUET_PATH.to_string(), PARQUET_PATH.to_string())
-            });
+    let update_row_counter_route = warp::path!("update-row-counter")
+        .and(warp::put())
+        .and_then(move || update_row_counter_handler(FILE_PATH.to_string(), FILE_PATH.to_string()));
 
     let display_dataframe_route =
         warp::path!("display-dataframe")
             .and(warp::get())
             .and_then(|| async move {
-                let parquet_path = PARQUET_PATH.to_string();
-                match read_parquet(&parquet_path).await {
+                match read_parquet(&FILE_PATH.to_string()).await {
                     Ok(df) => {
                         let df_json = serde_json::to_value(&df).unwrap();
                         Ok::<_, warp::Rejection>(warp::reply::json(&df_json))
